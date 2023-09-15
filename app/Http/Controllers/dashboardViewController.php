0d29c080afd7af146d16518956e9ec29256cfdd4
TonyVintachi <tonytmalete@gmail.com>
2023-09-15 09:22:19 +0200

    code to fix data bug

--- app/Http/Controllers/dashboardViewController.php
+++ app/Http/Controllers/dashboardViewController.php
@@ -14,4 +14,8 @@ public function index()
 $projects = DB::select('select * from projects');
 return view('dashboard',['projects'=>$projects]);
 }
+
+//ignore this https://www.geeksforgeeks.org/different-ways-for-passing-data-to-view-in-laravel/
+//swithing machince fixing a bug 
+
 }
